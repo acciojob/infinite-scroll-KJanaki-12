e1bb3b8dcda65f78d263f68c90d017673d6c7d26
KJanaki-12 <kenchugundujanaki@gmail.com>
2025-10-15 20:59:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 0;
+
+function addItems(n) {
+	for(let i=0;i<n; i++){
+		const li = document.createElement("li");
+		li.textContent = `Item ${++count}`;
+		list.appendChild(li);
+	}
+}
+
+addItems(10);
+
+window.addEventListener("scroll", ()=>{
+	if(window.innerHeight  + window.scrolly >= document.body.offsetHeight){
+		addItems(2);
+	}
+})
